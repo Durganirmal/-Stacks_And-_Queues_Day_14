b79d2a7ac1e00b26c072695836f6594e03e8b6f9
Durga Nirmal <durganirmal123@gmail.com>
2021-11-24 09:49:34 +0530

    Added : Program for the Peek and Pop the element till it's empty

--- src/com/Stacks_and_Queues/Operation.java
+++ src/com/Stacks_and_Queues/Operation.java
@@ -4,18 +4,28 @@ public class Operation {
 
 	public static Stacks_and_Queues addElement() {
 		Stacks_and_Queues stacklist =new Stacks_and_Queues();
-        stacklist.push(70);
+		stacklist.push(70);
         stacklist.push(30);
         stacklist.push(56);
 
         stacklist.display();
         return stacklist;
-
     }
 
+    public static void peek() {
+    	Stacks_and_Queues stacklist = new Stacks_and_Queues();
+        stacklist.push(70);
+        stacklist.push(30);
+        stacklist.push(56);
+        peek();
+    }
 
-}
-
+    public static void pop() {
+        System.out.println("Before Pop:");
+        Stacks_and_Queues stacklist = addElement();
+        pop();
+        System.out.println("After Pop:");
+        stacklist.display();
 	
-
-
+    }
+}
--- src/com/Stacks_and_Queues/Stack_Queues_main.java
+++ src/com/Stacks_and_Queues/Stack_Queues_main.java
@@ -3,18 +3,27 @@ package com.Stacks_and_Queues;
 import java.util.Scanner;
 public class Stack_Queues_main {
 
-	 public static void main(String[] args) {
-	        System.out.println("Welcome to the data Structure in 'Stack and Queue' Program!!");
-	        Scanner sc = new Scanner(System.in);
-	        System.out.println("Press '1' to add element at the start  ");
-	        switch (sc.nextInt()) {
-	            case 1:
-	                Operation.addElement();
-	                break;
-	            default:
-	            {
-	                System.out.println("Press Valid Key");
-	            }
-	        }
-	    }
-	}
+	public static void main(String[] args) {
+        System.out.println("Welcome to the data Structure in 'Stack and Queue' Program!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press '1' to add element at the start");
+        System.out.println("Press '2' to peek the element");
+        System.out.println("Press '3' to pop the element");
+        switch (sc.nextInt()) {
+            case 1:
+                Operation.addElement();
+                break;
+            case 2:
+                Operation.peek();
+                break;
+
+            case 3:
+                Operation.pop();
+                break;
+            default:
+            {
+                System.out.println("Press Valid Key");
+            }
+        }
+    }
+}
